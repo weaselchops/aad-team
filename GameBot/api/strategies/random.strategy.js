04acf444d22c7a21d53b5602c89427575a398350
Jens Golombek <jens.golombek@bertelsmann.de>
2020-12-09 11:40:57 +0100

    Change Bet to 1

--- GameBot/api/strategies/random.strategy.js
+++ GameBot/api/strategies/random.strategy.js
@@ -11,7 +11,8 @@ class RandomStrategy {
             // The bet of Humans that they will win
             // value of 1 means if Humans win, they will get 2x of the score - if they loose they loose 2x of the score
             // If null the Humans player does not support bets, value needs to be between 0 and 1
-            bet = Math.random();
+            //bet = Math.random();
+            bet = 1;
         }
 
         return {
